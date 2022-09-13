180c443daab6b8057234b297258f4199d1791b90
hoangson23 <khonghoangson2301@gmail.com>
2022-09-13 22:09:02 +0700

    routerredux

--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//redux thunk
+import { Provider } from 'react-redux';
+import thunk from "redux-thunk"
+import {applyMiddleware, createStore, compose} from "redux"
+import { rootReducer } from './redux/rootReducer';
+//copy paste hoac hoc thuoc
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store= createStore(
+  rootReducer,//rootreducer lay ben rootRedecer.js
+  composeEnhancers(applyMiddleware(thunk)))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
